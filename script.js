5603039ae68aeaa134322ee8dafe5013231c982e
Kieran <114930899+KizzaD91@users.noreply.github.com>
2023-03-01 10:23:58 +0000

    First commit for setup

---
+++ script.js
@@ -0,0 +1,44 @@
+// An array of objects containing quotes and the author
+const quotes = [
+  {
+    author: "Carol Burnett",
+    quote: "When you have a dream, you've got to grab it and never let go.",
+  },
+  {
+    author: "Martin Luther King, Jr.",
+    quote: "Out of the mountain of despair, a stone of hope.",
+  },
+  {
+    author: "Audrey Hepburn",
+    quote: "Nothing is impossible. The word itself says 'I'm Possible!'",
+  },
+  {
+    author: "Alexander the Great",
+    quote: "There is nothing impossible to they who will try."
+  },
+  {
+    author: "Michael Altshuler",
+    quote: "The bad news is time flies.  The good news is you're the pilot."
+  },
+  {
+    author: "Walt Whitman",
+    quote: "Keep your face always toward the sunshine, and shadows will fall behind you."
+  },
+  {
+    author: "Winston Churchill",
+    quote: "Success is not final, failure is not fatal: it is the courage to continue that counts."
+  },
+  {
+    author: "C.S. Lewis",
+    quote: "You are never too old to set another goal or to dream a new dream."
+  },
+  {
+    author: "Theodore Roosevelt",
+    quote: "Believe you can and you're halfway there."
+  },
+  {
+    author: "Roald Dahl",
+    quote: "If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely."
+  }
+];
+
