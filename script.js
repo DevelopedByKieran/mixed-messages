a07ad61194512d19d748fa100068a8612cb01bd2
Kieran <114930899+KizzaD91@users.noreply.github.com>
2023-03-01 11:02:18 +0000

    Make object names conventional

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
 // An array of objects containing quotes and the author
 const quotes = [
   {
-    author: "Carol Burnett",
-    quote: "When you have a dream, you've got to grab it and never let go.",
+    _author: "Carol Burnett",
+    _quote: "When you have a dream, you've got to grab it and never let go.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "Martin Luther King, Jr.",
-    quote: "Out of the mountain of despair, a stone of hope.",
+    _author: "Martin Luther King, Jr.",
+    _quote: "Out of the mountain of despair, a stone of hope.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "Audrey Hepburn",
-    quote: "Nothing is impossible. The word itself says 'I'm Possible!'",
+    _author: "Audrey Hepburn",
+    _quote: "Nothing is impossible. The word itself says 'I'm Possible!'",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "Alexander the Great",
-    quote: "There is nothing impossible to they who will try.",
+    _author: "Alexander the Great",
+    _quote: "There is nothing impossible to they who will try.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "Michael Altshuler",
-    quote: "The bad news is time flies.  The good news is you're the pilot.",
+    _author: "Michael Altshuler",
+    _quote: "The bad news is time flies.  The good news is you're the pilot.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "Walt Whitman",
-    quote: "Keep your face always toward the sunshine, and shadows will fall behind you.",
+    _author: "Walt Whitman",
+    _quote: "Keep your face always toward the sunshine, and shadows will fall behind you.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "Winston Churchill",
-    quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    _author: "Winston Churchill",
+    _quote: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "C.S. Lewis",
-    quote: "You are never too old to set another goal or to dream a new dream.",
+    _author: "C.S. Lewis",
+    _quote: "You are never too old to set another goal or to dream a new dream.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "Theodore Roosevelt",
-    quote: "Believe you can and you're halfway there.",
+    _author: "Theodore Roosevelt",
+    _quote: "Believe you can and you're halfway there.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   },
   {
-    author: "Roald Dahl",
-    quote: "If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.",
+    _author: "Roald Dahl",
+    _quote: "If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.",
     get fullQuote () {
-        return `${this.quote} - ${this.author}`;
+        return `${this._quote} - ${this._author}`;
     }
   }
 ];
